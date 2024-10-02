c2f2d8e9bf87fbf033740167f69543642b873c9a
justinozzy <jetoswald@gmail.com>
2024-10-02 16:15:29 -0700

    Added sorting methods + comments

--- main.py
+++ main.py
@@ -6,30 +6,46 @@ app = Flask(__name__)
 
 providers = IrsParser()
 
+"""
+Index page for website, renders the basic elements when the page is first loaded.
+Return type: String
+"""
 @app.route('/')
 def index():
     return render_template('index.html', states=States, providers=providers.provider_list)
 
+"""
+Endpoint for getting E-file provider information based on selected state and zipcode.
+Return type: String
+"""
 @app.route('/getproviders', methods=['GET'])
 def get_providers():
     zipCode = request.args.get('zipCode')
     state = request.args.get('state')
     providers.get_efile_providers(zipCode, int(state))
-    print(providers.provider_list)
     return render_template('index.html', states=States, providers=providers.provider_list)
 
+"""
+Endpoint for sorting E-file providers based on selected sorting option. Requires user to have already submitted
+a request for a state and zip.
+Return type: String
+"""
 @app.route('/sort', methods=['GET'])
 def sort_providers():
     sort_by = request.args.get('sort_by')
 
     # Sorting logic - sorts based on whatever the value of x is from the provider list
     match sort_by:
+        case 'bname':
+            sorted_providers = sorted(providers.provider_list, key=lambda x: x[0])
         case 'zip':
             sorted_providers = sorted(providers.provider_list, key=lambda x: x[2].split(" ")[-1])
-        case 'name':
-            sorted_providers = sorted(providers.provider_list, key=lambda x: x[0])
-        case 'contact':
-            sorted_providers = sorted(providers.provider_list, key=lambda x: x[3])
+        case 'fname':
+            sorted_providers = sorted(providers.provider_list, key=lambda x: x[3].split(" ")[0])
+        case 'lname':
+            sorted_providers = sorted(providers.provider_list, key=lambda x: x[3].split(" ")[1])
+        case 'phone':
+            sorted_providers = sorted(providers.provider_list, key=lambda x: x[4])
         case _:
             # Default return
             sorted_providers = providers.provider_list
